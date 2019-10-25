bb8156448b45d698140093ef888a56a0ab5ab5df
Ankur <ankur190300@gmail.com>
2019-10-26 02:09:50 +0530

    third commit: more routers and controller,as well as view engine set up

---
+++ controllers/users_controller.js
@@ -0,0 +1,3 @@
+﻿module.exports.profile = function (req, res) {
+    res.end("<h1>user profile</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
 
-console.log("router is loaded");
+console.log('router is loaded');
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
 module.exports = router;
